0efe075fbafd4df78038265cd9d79f338d6dd0a5
meo-albu <albumeo1990@gmail.com>
2021-08-16 10:16:26 +0300

    Category page tests

--- src/tests/category.test.tsx
+++ src/tests/category.test.tsx
@@ -2,7 +2,7 @@ import rootReducer from 'store/reducers'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-import { screen, render, act } from '@testing-library/react';
+import { screen, render, act, fireEvent } from '@testing-library/react';
 import Category from 'pages/category/[slug]'
 
 import {MockedProvider} from '@apollo/client/testing'
@@ -37,27 +37,78 @@ const mocks: any[] = [
     },
     result: {
       data: {
-        getWords: [{ german: 'ab', id: 'id1', translations: {english: 'from', romanian: 'din'} }]
+        getWords: [
+          { german: 'ab', id: 'id1', translations: {english: 'from', romanian: 'din'} },
+          { german: 'bevor', id: 'id2', translations: {english: 'before', romanian: 'inainte'} },
+        ]
       },
     },
   },
 ]
 
 describe('Category page', () => {
-  it('basic render', async () => {
 
-    render(
+  const Component = () => {
+    return (
       <MockProvider>
         <MockedProvider mocks={mocks} addTypename={false}>
           <Category slug='general' />
         </MockedProvider>
       </MockProvider>
     )
+  }
+
+  it('basic render', async () => {
+
+    render(<Component />)
     
     await act(async () => {
       await new Promise(resolve => setTimeout(resolve, 0))
     })
-    
+
     screen.getByText('ab')
+  })
+
+  it("should change the word on 'next' button click", async () => {
+
+    render(<Component />)
+    
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 0))
+    })
+
+    fireEvent.click(screen.getByRole('button', {name: 'next'}))
+    
+    screen.getByText('bevor')
+  })
+
+  it("should change the word on 'prev' button click", async () => {
+
+    render(<Component />)
+    
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 0))
+    })
+
+    fireEvent.click(screen.getByRole('button', {name: 'next'}))
+    screen.getByText('bevor')
+
+    fireEvent.click(screen.getByRole('button', {name: 'prev'}))
+    screen.getByText('ab')
+  })
+
+  it("the button should be disabled if first or last word is displayed", async () => {
+    render(<Component />)
+    
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 0))
+    })
+    expect(screen.getByRole('button', {name: 'prev'})).toBeDisabled()
+    expect(screen.getByRole('button', {name: 'next'})).not.toBeDisabled()
+    
+    fireEvent.click(screen.getByRole('button', {name: 'next'}))
+    fireEvent.click(screen.getByRole('button', {name: 'next'}))
+    expect(screen.getByRole('button', {name: 'next'})).toBeDisabled()
+    expect(screen.getByRole('button', {name: 'prev'})).not.toBeDisabled()
   })
 })
