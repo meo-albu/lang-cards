c4e24676794ff4a75e742ff6853dd76c23a5d8ca
meo-albu <albumeo1990@gmail.com>
2021-08-18 16:07:28 +0300

    add redux

--- __reactnative/App.tsx
+++ __reactnative/App.tsx
@@ -3,7 +3,36 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames'
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './store/reducers'
+
+import { useDispatch } from 'react-redux'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const store = createStore(rootReducer)
+
 export default function App() {
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  )
+}
+
+const Main = () => {
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    async function setLang() {
+      dispatch({
+        type: 'SET_LANG',
+        lang: await AsyncStorage.getItem('lang') ? await AsyncStorage.getItem('lang') : 'english'
+      })
+    }
+    setLang()
+  }, [dispatch])
+
   return (
     <View style={tw`flex-1`}>
       <Nav />
@@ -11,5 +40,5 @@ export default function App() {
         <Text>invata limba germana</Text>
       </View>
     </View>
-  );
+  )
 }
--- __reactnative/components/Nav.tsx
+++ __reactnative/components/Nav.tsx
@@ -2,17 +2,27 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import {Picker} from '@react-native-picker/picker'
 import tw from 'tailwind-react-native-classnames'
+import { useSelector, useDispatch } from 'react-redux'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { RootState } from '../store/reducers'
 
 export default function Nav() {
-  const [selectedLanguage, setSelectedLanguage] = React.useState('english')
+  const {langs, currentLang} = useSelector((state: RootState) => state.langReducer)
+
+  const dispatch = useDispatch()
+  
   return (
     <View style={tw`pt-10 w-full flex-row justify-end shadow-md bg-white`}>
       <Picker
         style={tw`pt-10 w-28`}
-        selectedValue={selectedLanguage}
-        onValueChange={(itemValue, itemIndex) =>
-          setSelectedLanguage(itemValue)
-        }
+        selectedValue={currentLang}
+        onValueChange={async(itemValue, itemIndex) => {
+          await AsyncStorage.setItem('lang', itemValue)
+          dispatch({
+            type: 'SET_LANG',
+            lang: itemValue
+          })
+        }}
       >
         <Picker.Item label="RO" value="romanian" />
         <Picker.Item label="EN" value="english" />
